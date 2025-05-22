33961e5b98339355ac52aa23c2c1c7d17bf07835
PixelLover <41162341+TJay12@users.noreply.github.com>
2025-05-22 17:10:59 +1200

    reading .json files commit

--- play_with_animals.py
+++ play_with_animals.py
@@ -2,5 +2,11 @@ from data_loader import load_json
 
 animals = load_json("animals.json")
 
+print(f"\nFour legged friends!")
 for animal, info in animals.items():
-    print(f"A {animal} goes '{info['sound']}' and has {info['legs']} legs")
+    if info['legs'] == 4:
+        print(f" - {animal}")
+
+print("")
+for animal, info in animals.items():
+    print(f"A {animal} goes '{info['sound']}'")
--- play_with_fruits.py
+++ play_with_fruits.py
@@ -4,3 +4,10 @@ fruits = load_json("fruits.json")
 
 for fruit, info in fruits.items():
     print(f"{fruit.title()} is {info['color']} and costs ${info['price']}")
+
+print(f"{'*' * 18}")
+print(f"{'Sale':^18}")
+print(f"{'*' * 18}")
+for fruit, info in fruits.items():
+    print(f"{fruit.title()}s only {info['price']}")
+print(f"{'*' * 18}")
